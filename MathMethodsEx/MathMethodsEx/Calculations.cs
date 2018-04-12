3cbda89c89788f9c842ff63b5d60c64b013ebd6f
Michael <mimomiller@gmail.com>
2018-04-12 16:52:15 -0700

    Initial commit of simple math methods

---
+++ MathMethodsEx/MathMethodsEx/Calculations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMethodsEx
+{
+    public static class Calculations
+    {
+        public static int multiplyByTwo(int number)
+        {
+            int result = number * 2;
+            return result;
+        }
+
+        public static int square(int number)
+        {
+            int result = number * number;
+            return result;
+        }
+
+        public static int ninetyNineMod(int number)
+        {
+            int result = 99 % number;
+            return result;
+        }
+    }
+}
---
+++ MathMethodsEx/MathMethodsEx/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MathMethodsEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer:");
+            bool valid = false;
+
+            while (!valid)
+            {
+                try
+                {
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    if(number != 0) // Accepts all integers except zero
+                        valid = true;
+                    int result1 = Calculations.multiplyByTwo(number);
+                    int result2 = Calculations.square(number);
+                    int result3 = Calculations.ninetyNineMod(number);
+                    Console.WriteLine("Your number times 2 is " + result1);
+                    Console.WriteLine("Your number squared is " + result2);
+                    Console.WriteLine("The remainder of 99 divided by your number is " + result3);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message + " Try again!");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
