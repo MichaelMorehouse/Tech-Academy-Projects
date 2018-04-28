ef6790002ab7ce9d633606ceda77f684dacc193b
Michael <mimomiller@gmail.com>
2018-04-27 17:08:45 -0700

    Initial commit of ASP.NET MVC comic books challenge

---
+++ FirstChallenge/FirstChallenge/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using FirstChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<ComicBook> comics = ComicBookManager.GetComicBooks();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(comics);
+        }
+
+        // GET: Detail
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ComicBook comic = comics.Find( x => x.ComicBookId == id);
+            if (comic == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comic);
+        }
+    }
+}
