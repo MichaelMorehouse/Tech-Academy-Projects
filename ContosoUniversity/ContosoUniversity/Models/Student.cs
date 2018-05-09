2e5b64ba96b0b8b6b5eb0eeec969b0cd2ab75325
Michael <mimomiller@gmail.com>
2018-05-09 15:18:26 -0700

    Add stored procedures for Department CRUD actions Implement TPH inheritance for Student and Instructor models Add optimistic concurrency handling for Department CRUD actions

--- ContosoUniversity/ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -7,29 +7,13 @@ using System.Web;
 
 namespace ContosoUniversity.Models
 {
-    public class Student
+    public class Student : Person
     {
-        public int ID { get; set; }
-        [StringLength(50, MinimumLength = 1)]
-        [Display(Name = "Last Name")]
-        public string LastName { get; set; }
-        [StringLength(50, MinimumLength = 1, ErrorMessage = "First name cannot be longer that 50 characters.")]
-        [Column("FirstName")]
-        [Display(Name = "First Name")]
-        public string FirstMidName { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
 
-        [Display(Name = "Full Name")]
-        public string FullName
-        {
-            get
-            {
-                return LastName + ", " + FirstMidName;
-            }
-        }
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
 }
